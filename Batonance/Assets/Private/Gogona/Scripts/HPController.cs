d041bed83179bd3131cadf8ba97ea970e3d16c1a
Gogona557 <97080960+TomitaHW21A084@users.noreply.github.com>
2023-05-09 12:57:56 +0900

    Add: HPを作成

--- Batonance/Assets/Private/Gogona/Scripts/HPController.cs
+++ Batonance/Assets/Private/Gogona/Scripts/HPController.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class HPController : MonoBehaviour
 {
+    [SerializeField, Tooltip("最大HP")]
+    private float maxHP;
+    [SerializeField, Tooltip("現在HP")]
+    private float currentHP;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // 現在HPを最大HPにする
+        currentHP = maxHP;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    /// <summary>
+    /// キャラクタのHPの取得
+    /// </summary>
+    /// <param name="hP"></param>
+    public void GetCharHP(float hP)
+    {
+        maxHP = hP;
     }
 }

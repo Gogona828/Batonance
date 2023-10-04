9b52b4796f35050346c48d3dd344c92e527536f0
UchinoKoki <hw21a023@oecu.jp>
2023-10-05 05:56:28 +0900

    Add:生成に関する大枠を提示、またリストの試験動作。（処理の重さ要確認）

--- Batonance/Assets/Private/Infill/Note/Debug_EnemySpawner.cs
+++ Batonance/Assets/Private/Infill/Note/Debug_EnemySpawner.cs
@@ -4,18 +4,23 @@ using UnityEngine;
 using NotesData;
 /// <summary>
 /// デバッグ用に動作させるためのスクリプト。NotesDataの読み込み発火が主。
-/// 機能集として使用。
+/// 機能集の備忘録として使用。
 /// </summary>
 public class Debug_EnemySpawner : MonoBehaviour
 {
     NoteDataUnpacker noteDataUnpacker;
-    public TextAsset text;
-    private (BaseData, NotesList) notesData;
+    public TextAsset text;//読み込むJsonFile
+    private (BaseData, NotesList) notesData;//デシリアライズデータの受け取り
+    private List<Notes> spawnEnemyData = new List<Notes>();
+
+    BGMManager bgmManager;
     // Start is called before the first frame update
     void Start()
     {
+        bgmManager = GameObject.Find("BGMManager").GetComponent<BGMManager>();
         noteDataUnpacker = this.GetComponent<NoteDataUnpacker>();
         Set();
+        AddSubjectListener();
     }
 
     // Update is called once per frame
@@ -34,6 +39,26 @@ public class Debug_EnemySpawner : MonoBehaviour
         // Debug.Log(notesData.Item2.notes[0].num);
 
         //配列型のリストへコンバート。for文を使用するため、処理のネックに注意
-        noteDataUnpacker.Convert_NoteListToList(notesData.Item2);
+        spawnEnemyData = noteDataUnpacker.Convert_NoteListToList(notesData.Item2);
+        //データの取り方（SpawnEnemeyData　配列２のノーツ、numを獲得する）
+        // Debug.Log(spawnEnemyData[2].num);
+    }
+    public void SpawnCheck()
+    {
+        Debug.Log("通知");
+        (int, int) measure = bgmManager.GetMeasure();
+        if(spawnEnemyData[0].num == measure.Item2)
+        {
+            //生成処理。
+            Debug.Log("Debug:生成処理");
+            spawnEnemyData.RemoveAt(0);
+            SpawnCheck();
+        
+        }
+    }
+    //拍を通知するUnityEventへの登録
+    public void AddSubjectListener()
+    {
+        bgmManager.subject.AddListener(SpawnCheck);
     }
 }

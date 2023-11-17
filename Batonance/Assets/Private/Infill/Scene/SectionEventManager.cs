9177e925215a0672ebf592def625f7ec809b5470
UchinoKoki <hw21a023@oecu.jp>
2023-11-17 14:15:03 +0900

    Add:セクション切り替えのマネージャーへオブジェクト名の確認を追加

--- Batonance/Assets/Private/Infill/Scene/SectionEventManager.cs
+++ Batonance/Assets/Private/Infill/Scene/SectionEventManager.cs
@@ -8,6 +8,13 @@ public class SectionEventManager : MonoBehaviour
     public UnityEvent subject = new UnityEvent();
     private int nowSection = 0;
     public int NowSection { get { return nowSection; }　private set { value = nowSection; } }
+    private void Start()
+    {
+        if(this.gameObject.name != "SectionEventManager")
+        {
+            Debug.LogWarning("Not the best object name");
+        }
+    }
     public void Initialize(int _nowSection)
     {
         NowSection = _nowSection;

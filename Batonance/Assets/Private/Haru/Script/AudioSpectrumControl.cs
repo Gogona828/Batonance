28691e0fb9d88c7a061449f11d1f393c962a1640
hw21a009 <hw21a009@oecu.jp>
2023-11-29 18:14:49 +0900

    Update: 画像を傾ける関数を作りました

--- Batonance/Assets/Private/Haru/Script/AudioSpectrumControl.cs
+++ Batonance/Assets/Private/Haru/Script/AudioSpectrumControl.cs
@@ -4,6 +4,23 @@ using UnityEngine;
 
 public class AudioSpectrumControl : MonoBehaviour
 {
+    public int debugint;//デバック用
+
+    [SerializeField]
+    private int[] angleList;//回転する角度を格納する配列
+    public static AudioSpectrumControl instance;
+    private void Awake()
+    {
+        //シングルトン
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +30,16 @@ public class AudioSpectrumControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //if (Input.GetKeyDown(KeyCode.I)) TurningSpectrum(0);//デバック用、左に傾ける
+        //if (Input.GetKeyDown(KeyCode.N)) TurningSpectrum(1);//デバック用、真ん中にする
+        //if (Input.GetKeyDown(KeyCode.S)) TurningSpectrum(2);//デバック用、右に傾ける
+    }
+    /// <summary>
+    /// オーディオスペクタルムの長い部分を動かす。左、真ん中、右の三種。次に敵が出てくる方向に向ける。
+    /// </summary>
+    /// <param name="directionsIndex">敵が出てくる方向を入力する。左＝0、真ん中＝1、右＝2</param>
+    public void TurningSpectrum(int directionsIndex)
+    {
+        transform.rotation = Quaternion.Euler(0, 0, angleList[directionsIndex]);
     }
 }
